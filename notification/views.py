885c2033946be6b779db63c2e3cab70fdd6b76eb
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-11-05 00:37:35 +0100

    setup done and added gitinore file

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+import secrets
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def home(request):
+    return render("home.html")
